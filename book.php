701f7ac23732f87ba05368ae42a4c4c43c79fb5c
jasperju <jasperjakob18@gmail.com>
2026-03-05 11:46:23 +0200

    05.03.26

--- book.php
+++ book.php
@@ -4,10 +4,61 @@ require_once('./connection.php');
 
 $id = $_GET['id'];
 
-// SELECT ...
-
 $stmt = $pdo->prepare('SELECT * FROM books WHERE id = :id');
 $stmt->execute(['id' => $id]);
-$user = $stmt->fetch();
+$book = $stmt->fetch();
 
-var_dump($book);
+$stmt = $pdo->prepare('SELECT * FROM authors WHERE id = :id');
+$stmt->execute(['id' => $id]);
+$author = $stmt->fetch();
+ 
+var_dump($author);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    <title><?= $book['title'] ?></title>
+</head>
+
+<body class="p-0">
+    <header>
+        <div class="p-10 pr-20 pl-20 flex flex-row justify-between border-b-1">
+            <div class="flex gap-20">
+                <a class="text-2xl" href="/">HOME</a>
+                <a class="text-2xl" href="/">BOOKS</a>
+            </div>
+            <div>
+                <a class="text-2xl" href="/">ACCOUNT</a>
+            </div>
+        </div>
+    </header>
+    <div class="flex justify-center items-center mt-20">
+        <div class="flex items-center flex-col gap-10">
+            <div class="flex flex-col gap-0 items-center">
+                <p class="font-semibold text-3xl max-w-prose">
+                    <?= $book['title']; ?> (<?= $book['release_date'] ?>)
+                </p>
+                <p class="font- text-2xl">
+                    <?= $author['first_name']; ?> <?= $author['last_name']; ?>
+                </p>
+            </div>
+            <img src="<?= $book['cover_path'] ?>" alt="">
+            <p class="text-xl font-semibold"><?= round($book['price'], 2); ?>€
+            </p>
+            <p class="max-w-prose text-lg">
+                <?= $book['summary']; ?>
+            </p>
+        </div>
+    </div>
+</body>
+
+</html>
+
+
+<!-- <?php while ($book = $stmt->fetch()) { ?>
+<?php } ?> -->
--- index.php
+++ index.php
@@ -2,27 +2,56 @@
 
 require_once('./connection.php');
 
-$stmt = $pdo->query('SELECT id, title FROM books');
-
+$stmt = $pdo->query('SELECT * FROM books');
+// tee paring sql andmebaasis et leida autor iga booki id jargi, igal raamatul on enda id kus on tema autorid, mitte koik autorid
 ?>
 
 <!doctype html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
     <title>Raamatupood</title>
 </head>
+
 <body>
-    <li>
-<?php while ($book = $stmt->fetch()) { ?>
-        <a href="book.php?id=<? echo $book['id']; ?>">
-            <?php echo $book['title']; ?>
-        </a>
-<?php } ?>
+    <header>
+        <div class="p-10 pr-20 pl-20 flex flex-row justify-between border-b-1">
+            <div class="flex gap-20">
+                <a class="text-2xl" href="/">HOME</a>
+                <a class="text-2xl" href="/">BOOKS</a>
+            </div>
+            <div>
+                <a class="text-2xl" href="/">ACCOUNT</a>
+            </div>
+        </div>
+    </header>
+    <?php while ($book = $stmt->fetch()) { ?>
+        <li>
+            <div class="flex justify-center items-center mt-20">
+                <div class="flex items-center flex-col gap-10">
+                    <div class="flex flex-col gap-0 items-center">
+                        <a href="book.php?id=<? echo $book['id']; ?>" class="font-semibold text-3xl max-w-prose">
+                            <?= $book['title']; ?> (<?= $book['release_date'] ?>)
+                        </a>
+                    </div>
+                    <img src="<?= $book['cover_path'] ?>" alt="">
+                    <p class="text-xl font-semibold"><?= round($book['price'], 2); ?>€
+                    </p>
+                    <p class="max-w-prose text-lg">
+                        <?= $book['summary']; ?>
+                    </p>
+                </div>
+            </div>
+        </li>
+    <?php } ?>
     </li>
 
 
+
 </body>
+
 </html>
 
